25a5d09ae783d1edec8c979458c0e948b19707c7
Jason Levitt <slimandslam@gmail.com>
2020-08-09 13:23:18 -0500

    build

--- src/index.js
+++ src/index.js
@@ -1,11 +1,14 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { Amplify } from 'aws-amplify';
+import { BrowserRouter as Router } from 'react-router-dom';
 import './index.css';
 import App from './App';
-import * as serviceWorker from './serviceWorker';
-import { BrowserRouter as Router } from 'react-router-dom';
-import { Amplify } from 'aws-amplify';
 import config from './config';
+import { initSentry } from './libs/errorLib';
+import * as serviceWorker from './serviceWorker';
+
+initSentry();
 
 Amplify.configure({
   Auth: {
