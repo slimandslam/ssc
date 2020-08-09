4a1180e84a7c4aa9c554dffcd29c1da49bb955d8
Jason Levitt <slimandslam@gmail.com>
2020-08-09 00:49:01 -0500

    frontend

---
+++ src/config.js
@@ -0,0 +1,16 @@
+export default {
+  s3: {
+    REGION: "us-east-1",
+    BUCKET: "notes2020demo"
+  },
+  apiGateway: {
+    REGION: "us-east-1",
+    URL: "https://1ib6x8m6oj.execute-api.us-east-1.amazonaws.com/prod"
+  },
+  cognito: {
+    REGION: "us-east-1",
+    USER_POOL_ID: "us-east-1_uPOqOzFn1",
+    APP_CLIENT_ID: "249rcabeio8cc819tmtr0n7n6v",
+    IDENTITY_POOL_ID: "us-east-1:28c52117-5ba0-4bab-8057-04a91f88674e"
+  }
+};
